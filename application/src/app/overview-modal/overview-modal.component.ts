d153e4fd76e846ebe6ceecd038c9dda024e1166d
Your Name <you@example.com>
2025-01-31 08:31:50 +0530

    changes

--- application/src/app/overview-modal/overview-modal.component.ts
+++ application/src/app/overview-modal/overview-modal.component.ts
@@ -13,43 +13,41 @@ export class OverviewModalComponent implements OnInit {
 	}
 
 	async ngOnInit() {
-		// const fetchData = await fetch(this.model.modelEndpoint.url, {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json',
-		// 		'api-key': this.model.modelEndpoint.key,
-		// 	},
-		// 	body: JSON.stringify({
-		// 		messages: [
-		// 			{
-		// 				role: "system",
-		// 				content: `You are a ${this.model.framework} architect.
-		// 				- Don't give code sample.`
-		// 			},
-		// 			{
-		// 				role: "user",
-		// 				content: [
-		// 					{
-		// 						type: "text",
-		// 						text: `The project is about building a web application using ${this.model.framework}. The project has table, chart, forms features. Give me 
-		// 						- Architecture Overview, 
-		// 						- Project Structure, 
-		// 						- Popular Libraries, 
-		// 						- Coding Standards
-		// 						- Sample Workflow`
-		// 					}
-		// 				]
-		// 			}
-		// 		],
-		// 		max_tokens: 1000,
-		// 		stream: false
-		// 	}),
-		// });
+		const fetchData = await fetch(this.model.modelEndpoint.url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'api-key': this.model.modelEndpoint.key,
+			},
+			body: JSON.stringify({
+				messages: [
+					{
+						role: "system",
+						content: `You are a ${this.model.framework} architect.
+						- Don't give code sample.`
+					},
+					{
+						role: "user",
+						content: [
+							{
+								type: "text",
+								text: `The project is about building a web application using ${this.model.framework}. The project has table, chart, forms features. Give me 
+								- Architecture Overview, 
+								- Project Structure, 
+								- Popular Libraries, 
+								- Coding Standards
+								- Sample Workflow`
+							}
+						]
+					}
+				],
+				max_tokens: 1000,
+				stream: false
+			}),
+		});
 
-		// const responseData = await fetchData.json();
-		// localStorage.setItem('overview', JSON.stringify(responseData));
-
-		this.markdown = JSON.parse(localStorage.getItem('overview')!).choices[0].message.content;
+		const responseData = await fetchData.json();
+		this.markdown = responseData.choices[0].message.content;
 	}
 
 }
